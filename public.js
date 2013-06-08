09d68d53e6a483cf6b687fd78b860febd396c4fc
alex-seville <alexander.southern@gmail.com>
2013-06-07 19:54:03 -0700

    Cleanup, adding live browser example

--- public.js
+++ public.js
@@ -1,37 +1,48 @@
-var jsdom, esprimaLib, exportVar, seen;
+var jsdom, esprimaLib, exportVar,
+ UNDEFINED='undefined',
+ OBJECT='object',
+ NATIVE_CODE='[native code]';
 
-if (typeof window === 'undefined'){
+//Determine if we're running this in the browser or not.
+if (typeof window === UNDEFINED){
     jsdom = require("jsdom").jsdom;
     esprimaLib = require("esprima");
     exportVar = exports;
 }else{
-    if (typeof esprima === 'undefined'){
+    if (typeof esprima === UNDEFINED){
         throw new Error("Missing Esprima dependency. esprima.org.");
     }
     esprimaLib = esprima;
     exportVar = window.publicjs = {};
 }
- var attachDependencies = function(deps,win){
+
+//Utility functions
+var attachDependencies = function(deps,win){
     deps.forEach(function(s){
         attachScript(s,win);
     });
- };
+};
 
- var attachScript = function(script,win){
+var attachScript = function(script,win){
     var scriptEl = win.document.createElement("script");
     scriptEl.text = script;
     win.document.body.appendChild(scriptEl);
- }
+};
 
-
-
+var isNativeCode = function(code){
+    //this should be a regex
+    return (
+        code.indexOf(NATIVE_CODE) === -1 &&
+        code.indexOf(" native function ") === -1
+    );
+};
 
 var parseFunction = function(fcn){
     try{
-        var parsed = esprimaLib.parse("("+fcn+")", { tolerant: true });
+        var parsed = esprimaLib.parse("(" + fcn + ")", { tolerant: true });
         return parsed.body[0].expression.params;
     }catch(e){
-        if (fcn.indexOf("[native code]") === -1 && fcn.indexOf(" native function ") === -1){
+        if (isNativeCode(fcn)){
             throw new Error(" ERROR parsing: "+fcn);
         }
     }
@@ -42,6 +53,15 @@ exportVar.getPublic = function(script,options){
         keys,newKeys,win,outputObj,included=[],
         offset,newVarSplit,createVar,currentObj,currentWindow,seen;
 
+        //if we want accessor errors to fail, we can do that here
+        //otherwise they get added to the tree
+        var accessorError = function(name,details){
+            if (options.throwErrors){
+                throw new Error("Accessor error on "+name+" of "+details);
+            }
+        };
+
+        //recursive method for generating parse tree
         var makeTree = function(obj){
             var tree=[],val;
             
@@ -52,53 +72,66 @@ exportVar.getPublic = function(script,options){
             //to avoid infinite recursion
             for(var i=0;i<seen.length;i++){
                 if (seen[i] == obj){
-                    tree.push({circular: true});
+                    tree.push({ circular: true });
                     return;
                 }
             }
-            
+            //save the fact that we've seen it now
             if (obj != {}){
                 seen.push(obj);
             }
 
             var keys = Object.keys(obj);
-            
+            //go through each of the enumerable keys
             keys.forEach(function(key){
                 try{
+                    //let's see if we can access the property
                     val = obj[key];
-                    var newobj = {
-                        name: key,
-                        type: typeof val
-                    };
-                    if (typeof val === 'object'){
-                        newobj.children = makeTree(val);
-                    }else if (typeof val === 'function'){
-                        newobj.params = parseFunction(val.toString());
-                        if (Object.keys(val).length > 0){
-                            newobj.children = makeTree(val);
-                        }
-                        if (val.prototype && 
-                            typeof val.prototype === 'object' &&
-                            val.prototype.constructor.toString().indexOf("[native code]") === -1){
-                            if (Object.keys(val.prototype).length > 0){
-                                newobj.prototype = makeTree(val.prototype);
-                            }
-                        }
-                    }else{
-                        newobj.value = val;
-                    }
-                    tree.push(newobj);
                 }catch(e){
-                    if (options.accessorErrors){
-                        throw new Error("Accessor error on "+key+" of "+obj);
+                    //we had a problem accessing the property,
+                    //let's handle it
+                    accessorError(key,obj);
+                    tree.push({
+                        name: key,
+                        type: 'accessor function'
+                    });
+                    return;
+                } 
+                //create a new leaf
+                var newobj = {
+                    name: key,
+                    type: typeof val
+                };
+                if (typeof val === OBJECT){
+                    newobj.children = makeTree(val);
+                }else if (typeof val === 'function'){
+                    //if it's a function we capture the parameters
+                    newobj.params = parseFunction(val.toString());
+                    if (Object.keys(val).length > 0){
+                        newobj.children = makeTree(val);
                     }
-                }   
+                    //and we capture anything that's living on the prototype
+                    if (val.prototype && 
+                          typeof val.prototype === OBJECT &&
+                          isNativeCode(
+                            val.prototype.constructor.toString()
+                          )
+                    ){
+                        if (Object.keys(val.prototype).length > 0){
+                            newobj.prototype = makeTree(val.prototype);
+                        }
+                    }
+                }else{
+                    newobj.value = val;
+                }
+                //and the leaf to the tree
+                tree.push(newobj);
             });
             return tree;
         };
 
     //create the window
-    win = typeof window !== 'undefined' ? window : jsdom().createWindow();
+    win = typeof window !== UNDEFINED ? window : jsdom().createWindow();
 
     //load dependencies
     if (options.dependencies && options.dependencies.length > 0){
@@ -129,13 +162,15 @@ exportVar.getPublic = function(script,options){
     newKeys.forEach(function(k){
         if (keys.indexOf(k) === -1){
             try{
+                //try accessing the property directly
                 outputObj[k] = win[k];
             }catch(e){
-                if (options.accessorErrors){
-                    throw new Error("Accessor error on "+k+" of "+win);
-                }
+                //there was an error accessing the property
+                accessorError(k,win);
             }  
         }else if (included.indexOf(k) > -1){
+            //we need to copy included variables, if present,
+            //and parse down
             offset = included.indexOf(k);
             //now we need to include the variable listed in
             //the include option
@@ -151,9 +186,7 @@ exportVar.getPublic = function(script,options){
                 try{
                     currentWindow = currentWindow[s];
                 }catch(e){
-                    if (options.accessorErrors){
-                        throw new Error("Accessor error on "+s+" of "+currentWindow);
-                    }
+                    accessorError(s,currentWindow);
                 }
             });
             currentObj[createVar]=currentWindow[createVar];
